7a5dc337d8cb9579c6b12557e4ae63ffc3c74a42
LightFuri <jobkuznetcov@gmail.com>
2024-03-22 13:05:16 +0300

    Fixed handler for cart.items.purchasePrice column

--- src/ExcelHandler.php
+++ src/ExcelHandler.php
@@ -133,6 +133,7 @@ class ExcelHandler implements BatchHandlerInterface
                                 break;
                             case 'cart.promotions.price':
                             case 'cart.promotions.total':
+                            case 'cart.items.purchasePrice':
                                 $pricing = $this->getRowFromCartPromotions($order->get('cart'), $field);
                                 $pricing = array_map(function ($value) {
                                     return $value / 100;
